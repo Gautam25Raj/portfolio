262d6e65a76556e50556188e472e223f846f71ec
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2023-01-29 00:03:35 +0530

    complete luffy text component

---
+++ src/Components/LuffyText.js
@@ -0,0 +1,33 @@
+import { motion, useScroll, useTransform } from 'framer-motion';
+import './LuffyText.css';
+
+const LuffyText = () => {
+  const { scrollYProgress } = useScroll();
+  console.log(scrollYProgress);
+  const x = useTransform(scrollYProgress, [0.45, 0.9], [-1000, -1200]);
+
+  return (
+    <div className="luffy-text-container">
+      <motion.div style={{ x }} className="transition-base no-select">
+        <div className="luffy-text luffy-text-border">
+          - Don't you dare make - Don't you dare make - Don't you dare make -
+          Don't you dare make
+        </div>
+        <div className="luffy-text luffy-text-color">
+          - my navigator cry! - my navigator cry! - my navigator cry! - my
+          navigator cry! - my navigator cry! -
+        </div>
+        <div className="luffy-text luffy-text-border">
+          - Monkey D. - Monkey D. - Monkey D. - Monkey D. - Monkey D. - Monkey
+          D. -
+        </div>
+        <div className="luffy-text luffy-text-color">
+          - Luffy - Luffy - Luffy - Luffy - Luffy - Luffy - Luffy - Luffy -
+          Luffy - Luffy -
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default LuffyText;
