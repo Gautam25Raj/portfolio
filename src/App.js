8cce808c359d4b9d684cb47cc99af091617e3db1
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2023-02-02 20:01:23 +0530

    Gsap install

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
+import Cursor from './Components/Cursor';
 import LuffyText from './Components/LuffyText';
 import Skills from './Layouts/Skills';
 import Footer from './Layouts/Footer';
 import Work from './Layouts/Work';
 
 import './App.css';
-import Cursor from './Components/Cursor';
 
 function App() {
   return (
-    <div className="App">
-      <Cursor />
-      <Skills />
-      <Work />
-      <LuffyText />
-      <Footer />
+    <div className="App" style={{ color: '#fff' }}>
+      {/* <Cursor /> */}
+      {/* <Skills /> */}
+      {/* <Work /> */}
+      {/* <LuffyText /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
