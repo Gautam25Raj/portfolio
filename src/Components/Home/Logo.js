f87fd3208ad41d9b0ec617f37305f20a01efdac8
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2023-02-08 22:23:41 +0530

    Complete Header Logo

---
+++ src/Components/Home/Logo.js
@@ -0,0 +1,19 @@
+import './Logo.css';
+
+import gr from './../../Assests/SVG/GR.svg';
+
+const Logo = () => {
+  return (
+    <div className="logo back-shadow height-border">
+      <img src={gr} alt="" className="gr"/>
+
+      <ul className="logo-list-container">
+        <li className="logo-list">Web Developer</li>
+        {/* <li className="logo-list">UI UX Designer</li>
+        <li className="logo-list">Software Engineer</li> */}
+      </ul>
+    </div>
+  );
+};
+
+export default Logo;
