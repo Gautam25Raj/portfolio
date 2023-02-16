4c6ee822a00aab8c2724253ffb3c64bda31875c8
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2023-02-17 00:03:13 +0530

    update ui and ux

--- src/Components/Home/Logo.js
+++ src/Components/Home/Logo.js
@@ -5,12 +5,12 @@ import gr from './../../Assests/SVG/GR.svg';
 const Logo = () => {
   return (
     <div className="logo back-shadow height-border">
-      <img src={gr} alt="" className="gr"/>
+      <img src={gr} alt="" className="gr" />
 
       <ul className="logo-list-container">
-        <li className="logo-list">Web Developer</li>
-        {/* <li className="logo-list">UI UX Designer</li>
-        <li className="logo-list">Software Engineer</li> */}
+        <li className="logo-list cursor-default">Web Developer</li>
+        {/* <li className="logo-list cursor-default">UI UX Designer</li>
+        <li className="logo-list cursor-default">Software Engineer</li> */}
       </ul>
     </div>
   );
--- src/Components/LuffyText.js
+++ src/Components/LuffyText.js
@@ -3,7 +3,7 @@ import './LuffyText.css';
 
 const LuffyText = () => {
   const { scrollYProgress } = useScroll();
-  const x = useTransform(scrollYProgress, [0.7, 0.95], [-1000, -1500]);
+  const x = useTransform(scrollYProgress, [0.8, 0.97], [-1000, -1500]);
 
   console.log(x);
 
